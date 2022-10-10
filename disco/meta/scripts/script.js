b98d1e375d9f62adf6416c27ea7f34fa8e86ca42
pieartsy <loviballet@yahoo.com>
2022-10-10 14:34:31 -0400

    added esc ability and roll result details

--- disco/meta/scripts/script.js
+++ disco/meta/scripts/script.js
@@ -1,3 +1,4 @@
+//adds continue block
 $(document).click(function(e) {
 	if($("span.continueblock").length) {
 		$("span.continueblock").first().removeClass('continueblock');
@@ -7,25 +8,38 @@ $(document).click(function(e) {
 });
 
 $(document).on("keyup", function (e) {
+  //pressing space key clicks continue button
+
   if ((e.keyCode == 32) && ($("#ContinueBtn"))) {
     $("#ContinueBtn").click();
   }
 
+  //pressing number keys selects options
   if ((e.keyCode >= 49 && e.keyCode <= 57) && (document.getElementById("OptionList"))) {
-    var list = document.getElementById("OptionList");
-    var childIndex = e.keyCode - 49; // 0 for '1', 1 for '2', etc
+    const list = document.getElementById("OptionList");
+    const childIndex = e.keyCode - 49; // 0 for '1', 1 for '2', etc
     if (list.children[childIndex]) {
       list.children[childIndex].getElementsByClassName("link-internal")[0].click();
     }
   }
 
-  if ((e.keyCode == 27) && (document.getElementById("menu-story"))) {
-    var menu = document.getElementById("menu-story");
-    menu.children[0].getElementsByClassName("link-internal")[0].click();
-  }
+  //pressing esc key
+  if ((e.keyCode == 27)) {
+    const dia = document.getElementById("ui-dialog");
+    var diastyle = window.getComputedStyle(dia).display;
+    console.log(diastyle);
 
-  if ((e.keyCode == 27) && (document.getElementById("#ui-overlay"))) {
-    var dia = document.getElementById("#ui-overlay");
-    console.log(dia)
+    // closes any open dialogs
+    if (document.getElementById("ui-dialog-close")) {
+      const diaclose = document.getElementById("ui-dialog-close");
+      diaclose.click();
+    }
+
+    //opens the overview if nothing is selected
+    if (diastyle == 'none') {
+      const menu = document.getElementById("menu-story");
+      console.log("test")
+      menu.children[0].getElementsByClassName("link-internal")[0].click();
+    }
   }
 });
