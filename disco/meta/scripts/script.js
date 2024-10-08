da081165e3dc3065ef3fc997e13724ea9b3219ec
Aster F <asterfialla@gmail.com>
2024-10-08 09:48:37 -0400

    scrolling document to bottom automatically

--- disco/meta/scripts/script.js
+++ disco/meta/scripts/script.js
@@ -6,13 +6,17 @@ $(document).click(function(e) {
 	return false;
 });
 
-postdisplay['paragraph'] = function (taskName) {
-	//$('.paragraph').hide().fadeIn(10000);
-};
+function scrolldown() {
+  const scrollingElement = (document.scrollingElement || document.body);
+  scrollingElement.scrollTop = scrollingElement.scrollHeight;
+}
 
 $(document).on("keyup", function (e) {
+  //pressing space key clicks continue button
+
   if ((e.keyCode == 32) && ($("#ContinueBtn"))) {
     $("#ContinueBtn").click();
+    scrolldown();
   }
 
   if ((e.keyCode >= 49 && e.keyCode <= 57) && (document.getElementById("OptionList"))) {
@@ -22,5 +26,20 @@ $(document).on("keyup", function (e) {
       list.children[childIndex].getElementsByClassName("link-internal")[0].click();
     }
   }
+
+  //pressing esc key
+  if ((e.keyCode == 27)) {
+    const dia = document.getElementById("ui-dialog");
+    var diastyle = window.getComputedStyle(dia).display;
+    console.log(diastyle);
+
+    // closes any open dialogs
+    if (document.getElementById("ui-dialog-close")) {
+      const diaclose = document.getElementById("ui-dialog-close");
+      diaclose.click();
+    }
+  }
 });
 
+// gets rid of the history/undo ability
+// Config.history.controls = false;
